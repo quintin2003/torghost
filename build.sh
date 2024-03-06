ff61b256b821f8c5ff2f62026979f0f875ed5fb6
quintin2003 <quintin2003@hotmail.ca>
2024-03-05 21:22:06 -0800

    final

--- build.sh
+++ build.sh
@@ -1,18 +1,18 @@
 echo "Torghost installer v3.0"
 echo "Installing prerequisites "
-sudo apt-get install tor python3-pip -y 
+sudo apt-get install tor pip3 iptables iptables-persistent python3-iptables python3-pip gcc gm2-12 -y
 echo "Installing dependencies "
 sudo pip3 install -r requirements.txt 
-mkdir build
-cd build
-cython3 ../torghost.py --embed -o torghost.c --verbose
+mkdir ./.build
+cd ./.build
+cython ../torghost.py --embed -o torghost.c --verbose
 if [ $? -eq 0 ]; then
     echo [SUCCESS] Generated C code
 else
     echo [ERROR] Build failed. Unable to generate C code using cython3
     exit 1
 fi
-gcc -Os -I /usr/include/python3.8 -o torghost torghost.c -lpython3.8 -lpthread -lm -lutil -ldl
+gcc -Os -I /usr/include/python3.* -o torghost torghost.c -lpython3.* -lpthread -lm -lutil -ldl
 if [ $? -eq 0 ]; then
     echo [SUCCESS] Compiled to static binay 
 else
